81199b3a4aa1bb6952e2d26b15e3fb37b2fc9d0e
Anub prasad <prasad@Anubs-MacBook-Air.local>
2025-08-04 22:39:13 +0530

    your message

--- BankingBackend/Backend.Repository/Interfaces/ITransactionRepository.cs
+++
@@ -1,8 +0,0 @@
-using Backend.DAL.Models;
-
-namespace Backend.Repository.Interfaces;
-public interface ITransactionRepository
-{
-    Task<Transaction> Create(Transaction transaction);
-    Task<List<Transaction>> GetByAccountNumber(string accountNumber);
-}
